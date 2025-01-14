d25daeff0b59d8ad019225c5f930299c48a6c6ff
akram ahil <akram.ahil123@gmail.com>
2025-01-14 16:25:18 +0100

    register

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -1,11 +1,11 @@
-import React  from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function Login() {
+export default function Register() {
   return (
     <section class="relative py-10 bg-gray-900 sm:py-16 lg:py-24">
     <div class="absolute inset-0">
-        <img class="object-cover w-full h-full" src="https://cdn.rareblocks.xyz/collection/celebration/images/signin/2/man-eating-noodles.jpg" alt="" />
+        <img class="object-cover w-full h-full" src="https://cdn.rareblocks.xyz/collection/celebration/images/signup/2/woman-working-laptop.jpg" alt="" />
     </div>
     <div class="absolute inset-0 bg-gray-900/20"></div>
 
@@ -13,15 +13,19 @@ export default function Login() {
         <div class="overflow-hidden bg-white rounded-md shadow-md">
             <div class="px-4 py-6 sm:px-8 sm:py-7">
                 <div class="text-center">
-                    <h2 class="text-3xl font-bold text-gray-900">Welcome back</h2>
-                    
-                    <p class="mt-2 text-base text-gray-600">Don’t have one? <Link to='/register' title="" class="text-orange-400 transition-all duration-200 hover:underline hover:text-orange-500">Create a free account</Link></p>
-                    
+                    <h2 class="text-3xl font-bold text-gray-900">Create an account</h2>
+                    <p class="mt-2 text-base text-gray-600">Already joined? <Link  to='/login' title="" class="text-orange-400 transition-all duration-200 hover:underline hover:text-orange-500">Sign in now</Link></p>
                 </div>
-
 
                 <form action="#" method="POST" class="mt-8">
                     <div class="space-y-5">
+                        <div>
+                            <label for="" class="text-base font-medium text-gray-900"> First & Last name </label>
+                            <div class="mt-2.5">
+                                <input type="text" name="" id="" placeholder="Enter your full name" class="block w-full p-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
+                            </div>
+                        </div>
+
                         <div>
                             <label for="" class="text-base font-medium text-gray-900"> Email address </label>
                             <div class="mt-2.5">
@@ -30,22 +34,21 @@ export default function Login() {
                         </div>
 
                         <div>
-                            <div class="flex items-center justify-between">
-                                <label for="" class="text-base font-medium text-gray-900"> Password </label>
-
-                               </div>
+                            <label for="" class="text-base font-medium text-gray-900"> Password </label>
                             <div class="mt-2.5">
                                 <input type="password" name="" id="" placeholder="Enter your password" class="block w-full p-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
                             </div>
                         </div>
 
                         <div>
-                            <button type="submit" class="inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-white transition-all duration-200 bg-blue-400 border border-transparent rounded-md focus:outline-none hover:bg-blue-500 focus:bg-blue-700">Log in</button>
+                            <button type="submit" class="inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-white transition-all duration-200 bg-blue-400 border border-transparent rounded-md focus:outline-none hover:bg-blue-500 focus:bg-blue-600">Sign up</button>
                         </div>
 
                         
                     </div>
                 </form>
+
+                
             </div>
         </div>
     </div>
