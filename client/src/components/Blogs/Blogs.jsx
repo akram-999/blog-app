cd26fb103c6a04fa41bd5866dc13d5ee2def9104
akram ahil <akram.ahil123@gmail.com>
2025-01-27 16:33:49 +0100

    BlogsHome

--- client/src/components/Blogs/Blogs.jsx
+++ client/src/components/Blogs/Blogs.jsx
@@ -6,52 +6,17 @@ import { useState, useEffect } from 'react';
 
 export default function Blogs({blogs}) {
   const API_URL = "http://localhost:5000/api/categories";
-  const [categories, setCategories] = useState([]);
-
-  useEffect(() => {
-    const fetchCategories = async () => {
-      const res = await axios.get(`${API_URL}`);
-      setCategories([{ _id: 'all', name: 'All' }, ...res.data]);
-    };
-    fetchCategories();
-  }, []);
-
-  const [selectedCategory, setSelectedCategory] = useState('All');
-  const handleCategoryClick = (category) => {
-    setSelectedCategory(category);
-  };
-
-  const filteredBlogs = selectedCategory === 'All' 
-    ? blogs 
-    : blogs.filter(blog => blog.categories.some(category => category === selectedCategory));
-
+ 
   
   
    return (
     <section className='mt-10'>
        
             <div className='mt-10'>
-                <div className='mt-10'>
-                  <ul className='flex items-center justify-around '>
-                    {categories.map((category) => (
-                      <li 
-                        className={`cursor-pointer py-1 px-3 rounded-full ${
-                          selectedCategory === category.name
-                            ? 'text-white bg-gradient-to-tr from-orange-600 to-orange-300'
-                            : 'text-gray-700 bg-gray-100 hover:bg-gray-200'
-                        }`}
-                        key={category._id} 
-                        onClick={() => handleCategoryClick(category.name)}
-                      >
-                        {category.name}
-                      </li>
-                    ))}
-                    
-                  </ul>
-                </div>
+                
 
                 <div className='mt-10 mb-5 mx-9 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
-                  {filteredBlogs.slice(0, 8).map((product) => (
+                  {blogs.slice(0, 8).map((product) => (
                     <article className="group blog" key={product.id} >
                     <img src={product.photo ? `http://localhost:5000/images/${product.photo}` : "default-avatar.png"}
                       className="h-56 w-full rounded-xl object-cover shadow-xl transition group-hover:grayscale-[50%]"
